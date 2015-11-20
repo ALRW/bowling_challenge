66a2667ce84a8f958a37c990640ae35a2073b512
George Maddocks <georgemaddocks@yahoo.co.uk>
2015-11-20 14:56:02 +0000

    game logic largely finished

--- src/game.js
+++ src/game.js
@@ -6,7 +6,7 @@ var frames;
 var length;
 var bonusScores;
 
-function Game (player) {
+function Game() {
   this.frames = [];
   this.bonusScores = [];
 }
@@ -17,36 +17,47 @@ Game.prototype = {
   },
   storeScores: function(array){
     this.frames.push(array);
+    if(this.getFrames().length > 1){
+      this.calculateBonusScore();}
   },
   runningScore: function(){
     var flattened = [].concat.apply([],this.frames);
     return flattened.reduce(function(a,b) {return a + b;});
   },
 
-  _frameLength: function(){
+  frameLength: function(){
     return this.frames.length;
   },
 
   calculateBonusScore: function(){
       if (this._isStrike()) {
-        this.bonusScores.push(this.frames[this._frameLength() -1][0] + this.frames[this._frameLength() -1][1]);
+        this.bonusScores.push(this.frames[this.frameLength() -1][0] +
+        this.frames[this.frameLength() -1][1]);
       }
       else if (this._isSpare()){
-        this.bonusScores.push(this.frames[this._frameLength() - 1][0]);
+        this.bonusScores.push(this.frames[this.frameLength() - 1][0]);
       }
+  },
+
+  calculateTotalScore: function(){
+    return this.runningScore() + this._getBonusTotal();
   },
 
   getBonusScores: function(){
     return this.bonusScores;
   },
 
+  _getBonusTotal: function() {
+    return this.getBonusScores().reduce(function(a,b) {return a + b;});
+  },
+
   _isSpare: function(){
-    return (this.frames[this._frameLength() - 2][0] +
-      this.frames[this._frameLength() - 2][1] === 10);
+    return (this.frames[this.frameLength() - 2][0] +
+      this.frames[this.frameLength() - 2][1] === 10);
   },
 
   _isStrike: function(){
-    return (this.frames[this._frameLength() - 2][0] === 10);
+    return (this.frames[this.frameLength() - 2][0] === 10);
   }
 
 
